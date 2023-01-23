3a849d489c5f517172f86945c952e677f242ca26
divie2 <108797237+divie2@users.noreply.github.com>
2023-01-23 21:27:19 +0100

    second commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int n;
+    int x;
+
+    n = 5+2*9/3-3;
+
+    // x = n + 12;
+
+    printf("%d" ,n);
+    printf("\nHello world");
+    printf("\nGoal!!");
+
+    return 0;
+}
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
 print('helloWorld')
+n = int(True)
+
+a = 1
+b = 2
+c= 1
+
+if n == 1 or b == 1:
+    print("y")
+
+
+
